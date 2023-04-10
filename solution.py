167054867f1bb1c74c2c8af295c4a0ef3b20c71d
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-10 13:29:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,5 @@ from scipy.stats import ttest_1samp
 chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool:
-    alpha = 0.05
-    t_stat, p_value = ttest_1samp(x, 300)
-    return p_value/2 < alpha and t_stat < 0
+    return stats.ttest_ind(x, y)[1] < 0.05 # Ваш ответ, True или False
     
