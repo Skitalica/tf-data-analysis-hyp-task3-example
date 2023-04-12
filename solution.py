3fbcb27a391ca35be4697d943aba33416c0a8e48
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-12 15:23:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_1samp
+from scipy.stats import ttest_ind
+from scipy.stats import permutation_test
 
 chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool:
-    return stats.ttest_ind(x, y)[1] < 0.05 # Ваш ответ, True или False
-    
+def solution(x: np.array, y: np.array) -> bool:
+    _, p_value = ttest_ind(x, y, equal_var=False, permutations=5000, alternative='less')
+    alpha = 0.05
+    return p_value < alpha
