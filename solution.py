cb803060787a4d0be9786816e164b1dfce5fb2f8
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-12 15:54:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,10 @@ from scipy.stats import permutation_test
 
 chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array, y: np.array) -> bool:
-    _, p_value = ttest_ind(x, y, equal_var=False, permutations=5000, alternative='less')
+def solution(x: np.array, y: np.array) -> bool: 
+    p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=5000,
+                 alternative='greater').pvalue 
     alpha = 0.05
     return p_value < alpha
